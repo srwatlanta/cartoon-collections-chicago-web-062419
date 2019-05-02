24832d8eb295d4270d71bcf389029c4e333e8a79
Shawn Wheeler <shawnrwheeler@gmail.com>
2019-05-02 01:38:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |food|
-    food == cheese_types
+    food == "cheddar" || "gouda" || "camembert"
   end
 end
